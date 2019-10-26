5bf983ba3e304cbc3622c7d35feb8d643ccf4e90
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-26 18:47:57 +0200

    Async

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ function getTrees() {
   http.onload = () => {
     $("#num").text(parseInt(http.responseText).toLocaleString());
     $("#diff").text((20000000-parseInt(http.responseText)).toLocaleString());
+    return http.responseText;
+
   };
-  return http.responseText;
 }
 var start = getTrees();
 console.log(start);
