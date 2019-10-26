5fc43f23967983f1d1f94332b30057acc4e7ffb6
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-26 11:37:23 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ import time
 from bs4 import BeautifulSoup
 import yagmail
 from http.server import BaseHTTPRequestHandler
+from datetime import datetime
 
 class handler(BaseHTTPRequestHandler):
 
@@ -10,26 +11,8 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write("Hello world").encode())
+    self.wfile.write(str("Hello world").encode())
     return
-
-
-def HTTP_C():
-	# Set the URL you want to webscrape from
-	url = 'https://teamtrees.org'
-
-	print("Making a request to",url)
-
-	# Connect to the URL
-	response = requests.get(url)
-	print("Got index")
-	# Parse HTML and save to BeautifulSoup object
-	soup = BeautifulSoup(response.text, "html.parser")
-
-	for cell in soup.select('#totalTrees'):
-		print(cell["data-count"] + " trees planted!")
-		HTTP_C(cell["data-count"])
-		return cell["data-count"]
 
 
 
