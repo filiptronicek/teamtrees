010024101462d724f8a03c18d82c014f2c452406
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-26 19:46:21 +0200

    animate

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
+var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',');
+
 function getTrees() {
   const http = new XMLHttpRequest();
 
-  http.open("GET", "./api/trees");
+  http.open("GET", "./api/trees.txt");
   http.send();
 
   http.onload = () => {
-    $("#num").text(parseInt(http.responseText).toLocaleString());
-    $("#diff").text((20000000-parseInt(http.responseText)).toLocaleString());
+    var diff = 20000000 - parseInt(http.responseText);
+    $("#num").animateNumber({
+      number: http.responseText,
+      numberStep: comma_separator_number_step
+    }, {
+      duration: 1500
+    });
+    $("#diff").animateNumber({
+      number: diff,
+      numberStep: comma_separator_number_step
+    }, {
+      duration: 1500
+    });
     return http.responseText;
-
   };
 }
 
 getTrees();
 setInterval(function() {
-    getTrees();
+  getTrees();
 }, 2000);
