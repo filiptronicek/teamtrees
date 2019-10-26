b631a9c912c518ffaefd98644af7afd2585e2687
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-26 11:44:26 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -13,3 +13,20 @@ class handler(BaseHTTPRequestHandler):
     self.wfile.write(str("Hello world").encode())
     return
 
+def Get_Trees():
+
+  # Set the URL you want to webscrape from
+  url = 'https://teamtrees.org'
+
+ # print("Making a request to",url)
+
+  # Connect to the URL
+  response = requests.get(url)
+  print("Got index")
+
+  # Parse HTML and save to BeautifulSoup object
+  soup = BeautifulSoup(response.text, "html.parser")
+
+  for cell in soup.select('#totalTrees'):
+    #print(cell["data-count"] + " trees planted!")
+    return cell["data-count"]
