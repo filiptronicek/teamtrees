28c65146b47353e1b25d40077847cc7398e67b2d
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-26 10:49:28 +0200

    Update test.py

--- api/test.py
+++ api/test.py
@@ -7,5 +7,5 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    self.wfile.write(str("Hello world").encode())
     return
