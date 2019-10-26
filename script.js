a75b4b5d360ed4bca5838f606bcbe239add31b8a
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-26 18:23:43 +0200

    Log

--- script.js
+++ script.js
@@ -7,10 +7,8 @@ function getTrees() {
 
   http.onload = () => {
     $("#num").text(parseInt(http.responseText).toLocaleString());
-    console.log("We at: "+parseInt(http.responseText));
-    console.log(20000000-parseInt(http.responseText));
     $("#diff").text((20000000-parseInt(http.responseText)).toLocaleString());
-    //console.log(parseInt(start)-parseInt(http.responseText));
+    console.log(parseInt(start)-parseInt(http.responseText));
   };
   return parseInt(http.responseText);
 }
