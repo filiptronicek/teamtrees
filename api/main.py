5beba109765480ec3d3a384e8363808caafa9d3a
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-26 11:34:30 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -4,6 +4,16 @@ from bs4 import BeautifulSoup
 import yagmail
 from http.server import BaseHTTPRequestHandler
 
+class handler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write("Hello world").encode())
+    return
+
+
 def HTTP_C():
 	# Set the URL you want to webscrape from
 	url = 'https://teamtrees.org'
@@ -21,14 +31,6 @@ def HTTP_C():
 		HTTP_C(cell["data-count"])
 		return cell["data-count"]
 
-class handler(BaseHTTPRequestHandler):
-
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write("Hello world").encode())
-    return
 
 
 
