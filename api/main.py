7e7063d13e7d0db0020968ab99557ff2d810f3d9
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-26 10:56:28 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -4,29 +4,28 @@ from bs4 import BeautifulSoup
 import yagmail
 from http.server import BaseHTTPRequestHandler
 
-# Set the URL you want to webscrape from
-url = 'https://teamtrees.org'
+def do_GET(self):
+	# Set the URL you want to webscrape from
+	url = 'https://teamtrees.org'
 
-print("Making a request to",url)
+	print("Making a request to",url)
 
-# Connect to the URL
-response = requests.get(url)
-print("Got index")
+	# Connect to the URL
+	response = requests.get(url)
+	print("Got index")
 
-emailIndex = 0
+	emailIndex = 0
 
-# Parse HTML and save to BeautifulSoup object
-soup = BeautifulSoup(response.text, "html.parser")
+	# Parse HTML and save to BeautifulSoup object
+	soup = BeautifulSoup(response.text, "html.parser")
 
-for cell in soup.select('#totalTrees'):
-	print(cell["data-count"] + " trees planted!")
-	class handler(BaseHTTPRequestHandler):
-
-		def do_GET(self):
-			self.send_response(200)
-			self.send_header('Content-type', 'text/plain')
-			self.end_headers()
-			self.wfile.write(cell["data-count"].encode() + " trees planted!".encode())
-			return
+	for cell in soup.select('#totalTrees'):
+		print(cell["data-count"] + " trees planted!")
+		class handler(BaseHTTPRequestHandler):		
+				self.send_response(200)
+				self.send_header('Content-type', 'text/plain')
+				self.end_headers()
+				self.wfile.write(cell["data-count"].encode() + " trees planted!".encode())
+				return
 
 
