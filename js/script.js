9b0d841444484c537c5e054a2fb027866859b5b9
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-27 15:11:58 +0100

    Down

--- js/script.js
+++ js/script.js
@@ -1,48 +1,53 @@
-var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',');
+var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(
+  ","
+);
 init = true;
 aDuration = 3000;
-function getTrees() {
 
+console.log("getting ping");
+$.get("./api/ping", function(
+  data
+) {
+  console.log(data);
+});
+function getTrees() {
+  location.href="down.html"
   const http = new XMLHttpRequest();
 
-  const ping = new XMLHttpRequest();
 
-  ping.open("GET", "./api/ping ");
-  ping.send();
-  ping.onload = () => {
-    if(ping.responseText != "200") {
-      console.log("Status code is: " + ping.responseText);
-    } else {
-      console.log(ping.responseText);
-      http.onload = () => {
-        //http.open("GET", "./api/index.txt ");
-        http.open("GET", "./api/ ");
-        http.send();
-      
-        http.onload = () => {
-          var diff = 20000000 - parseInt(http.responseText);
-          $("#num").animateNumber({
-            number: http.responseText,
-            numberStep: comma_separator_number_step
-          }, {
-            duration: aDuration
-          });
-          $("#diff").animateNumber({
-            number: diff,
-            numberStep: comma_separator_number_step
-          }, {
-            duration: aDuration
-          });
-          return http.responseText;
-        };
-      
-      };
-    }
+  http.onload = () => {
+    //http.open("GET", "./api/index.txt ");
+    console.log("Getting numbers");
+    http.open("GET", "./api/ ");
+    http.send();
+
+    http.onload = () => {
+      var diff = 20000000 - parseInt(http.responseText);
+      $("#num").animateNumber(
+        {
+          number: http.responseText,
+          numberStep: comma_separator_number_step
+        },
+        {
+          duration: aDuration
+        }
+      );
+      $("#diff").animateNumber(
+        {
+          number: diff,
+          numberStep: comma_separator_number_step
+        },
+        {
+          duration: aDuration
+        }
+      );
+      return http.responseText;
+    };
   };
-  
+}
 
 getTrees();
 init = false;
 setInterval(function() {
   getTrees();
-}, aDuration)}
+}, aDuration);
