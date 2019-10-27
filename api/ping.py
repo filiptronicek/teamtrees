aa4e56d7d928dba6dd1e5b08a9ef1622f0dedb86
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-27 14:55:57 +0100

    Encoding

--- api/ping.py
+++ api/ping.py
@@ -5,7 +5,7 @@ class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
+    self.send_header('Content-type', 'text/plain;charset=utf-8')
     self.end_headers()
     code = pingSite()
     self.wfile.write(str(code).encode())
