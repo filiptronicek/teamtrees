fe88ad8ea6cca75e55770826b4febc1b6626f2b1
Filip Troníček <filip.tronicek@seznam.cz>
2019-12-05 22:26:05 +0100

    Durations

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let comma_separator_number_step = $.animateNumber.numberStepFactories.separator(",");
-let aDuration = 1000;
-let refreshDuration = 3000;
+let aDuration = 4000;
+let refreshDuration = 8000;
 let acc = document.getElementsByClassName("accordion");
 let i;
 /*
